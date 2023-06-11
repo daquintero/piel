6f4983e22dda072786f2777b173b1132361840bf
Dario Quintero <darioaquintero@gmail.com>
2023-06-11 13:17:13 +0100

    FEAT: Automated PYPI publication

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/daquintero/piel',
-    version='0.0.2',
+    version='0.0.13',
     zip_safe=False,
 )
