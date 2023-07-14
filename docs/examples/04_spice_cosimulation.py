0b9a0064292062a2dec17eb35428ab50f695e45e
Dario Antonio Quintero Dominguez <darioaquintero@gmail.com>
2023-07-14 20:31:41 +0100

    FEAT: Going to port into hdl21 instead of pyspice (#16)

--- docs/examples/04_spice_cosimulation.py
+++ docs/examples/04_spice_cosimulation.py
@@ -1,4 +1,4 @@
-# # Multi-Domain Interconnect
+# # Further Analogue-Enhanced Cosimulation including `SPICE`
 
 # This example demonstrates the modelling of multi-physical component interconnection and system design.
 
@@ -155,29 +155,32 @@ print(spice_circuit)
 # ```
 # .title straight_heater_metal_s_b8a2a400
 # R0 straight_1__e1___taper_1__e2 straight_1__e2___taper_2__e2 10kOhm
-# R1 taper_1__e1___via_stack_1__e3 out 10kOhm
 # R2 straight_1__e2___taper_2__e2 taper_2__e1___via_stack_2__e1 10kOhm
-# R3 taper_1__e1___via_stack_1__e3 out 10kOhm
-# R4 taper_2__e1___via_stack_2__e1 out 10kOhm
 # ```
 
 # We can now simulate so much more. Note that this API is WIP.
 
+spice_circuit
+
 # We can extract the electrical components of our heater implementation on its own first.
-
-import sax
-
-sax.get_required_circuit_models(our_resistive_heater_netlist)
-
-a = sax.extract_circuit_elements(our_resistive_heater_netlist)
-nodes = [n for n, d in a.out_degree() if d == 0]
-nodes
-
-a.nodes
 
 # ## `PySpice` Integration
 
 # We have seen in the previous example how to integrate digital-driven data with photonic circuit steady-state simulations. However, this is making a big assumption: whenever digital codes are applied to photonic components, the photonic component responds immediately. We need to account for the electrical load physics in order to perform more accurate simulation models of our systems.
 #
 # `piel` provides a set of basic models for common photonic loads that integrates closely with `gdsfactory`. This will enable the multi-domain co-design we like when integrated with all the open-source tools we have previously exemplified. You can find the list and definition of the provided models in:
+#
+
+# ### Creating our Stimulus
+
+# Let's first look into how to map a numpy data into a SPICE waveform. The interconnect of electrical stimulus will be done specifically through standard numpy arrays. One of the main complexities of inputting the SPICE signal information is that SPICE solvers are designed to input specific types of signals such as sine waves, step responses, pulses, squares, sawtooth waves with parameters defined for those signals. However, we might want to explore how custom signals affect our circuit, say in a particular modulation regime. Adding non-standard waves is custom behaviour in some SPICE solvers. However, this can be done in the form of a "Piecewise Linear Controlled Source" as in the 12.2.7 section of the [NGSpice documentation](https://ngspice.sourceforge.io/docs/ngspice-manual.pdf.). One of the complexities is that `PySpice` is not using the latest version of NGSPice. For the sake of simplicity, we will start with the pre-built signal sources.
+
+# ### Mixed-Signal Electronic Photonic Simulation Methodology
+
+# We can now simulate SPICE-based circuits alongside our defined layout electrical models. This allows us the power to implement our own sources, and our own components in a more complete manner. However, this does not fundamentally solve the problem we have on multiple time domain simulations. We have timing data from digital sources and analog electronic sources. If you have delayed photonic signals, then we also have timing data independently in the photonic domain. This leads to a major time-synchronisation issue. It is in this type of problem structure that a microservice implementation again comes to the rescue.
+#
+# For example, digital signals are particularly valuable and useful when considering steady-state signal propagation. Analogue signals are particularly interesting in the transition between electronic states, as the rise-times and signal-shapes will be determined by RC constants primarily. In the midst of all of this, photonic signals can change independently. There is a direct control from the electronics to the unitary of the component, and at discretised points in time the unitary can be computed. For the period of discrete time this signal is valid, the unitary can be computed. The accuracy desired is just determined by the discretization time. In that period of time, there is a linear relationship between optical signal inputs, and outputs.
+#
+# As an approximation, if the signals are switching between logic levels, then it is reasonable to compute the analog rise-time and fall-time signals accordingly and operate on them linearly if the digital clock period is high. However, if the logic levels in which the signal is switching is
+
 #
