fd41c418f2507e8cba41bf8bc8d6baabf31be0b5
Dario Quintero <darioaquintero@gmail.com>
2023-09-15 15:05:06 +0100

    FEAT: Update to pyproject.toml

--- environment/nix/shell.nix
+++ environment/nix/shell.nix
@@ -1,40 +1,35 @@
-{ pkgs ? import <nixpkgs> {}
-}:
-
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.stdenv.cc.cc.lib
-    pkgs.jupyter
-    pkgs.which
-    pkgs.htop
-    pkgs.zlib
-    pkgs.pandoc
-    pkgs.ngspice # 41 (latest)
-    pkgs.gtkwave # 3.3.117, from Aug 2023 (latest)
-    pkgs.xyce # 7.6, from Nov 2022 (7.7 is latest)
-    pkgs.verilog # 12.0, from Jun 2023 (latest)
-    pkgs.python3Packages.virtualenv # run virtualenv .
-    pkgs.python3Packages.numpy
+# An example package with dependencies defined via pyproject.toml
+{
+  config,
+  lib,
+  dream2nix,
+  pkgs ? import <nixpkgs> {}
+}: let
+  pyproject = lib.importTOML (config.mkDerivation.src + /pyproject.toml);
+in {
+  imports = [
+    dream2nix.modules.dream2nix.pip
   ];
 
-  nativeBuildInputs = [
-    pkgs.autoPatchelfHook
-  ];
+  inherit (pyproject.project) name version;
 
-  shellHook = ''
-    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/ # fixes libstdc++ issues and libgl.so issues
-    if [ -e ../../.venv/bin/activate ];
-     then source ../../.venv/bin/activate;
-    else
-      pip install --upgrade pip;
-      python -m venv ../../.venv;
-      source ../../.venv/bin/activate;
-      pip install -r ../../requirements_dev.txt;
-      pip install -r ../../../openlane2/requirements_dev.txt;
-      pip install -e ../../;
-      source ../../.venv/bin/activate;
-    fi
-    nix-shell ../../../openlane2/shell.nix
-  '';
-  LOCALE_ARCHIVE="/usr/lib/locale/locale-archive";  # let's nix read the LOCALE, to silence warning messages
+  mkDerivation = {
+    src = ./.;
+  };
+
+  buildPythonPackage = {
+    format = lib.mkForce "pyproject";
+    pythonImportsCheck = [
+      "my_tool"
+    ];
+  };
+
+  pip = {
+    pypiSnapshotDate = "2023-08-27";
+    requirementsList =
+      pyproject.build-system.requires
+      or []
+      ++ pyproject.project.dependencies;
+    flattenDependencies = true;
+  };
 }
