eb0577fed2dc7b839762ebdfc89b5d6b69b6b088
daquintero <darioaquintero@gmail.com>
2024-10-19 12:17:56 +0200

    Let's try this

--- scripts/install_piel_nix.sh
+++ scripts/install_piel_nix.sh
@@ -1,7 +1,8 @@
- curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix/pr/1145 | sh -s -- install --no-confirm --extra-conf "
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix/pr/1145 | sh -s -- install --no-confirm --extra-conf "
     extra-substituters = https://openlane.cachix.org
     extra-trusted-public-keys = openlane.cachix.org-1:qqdwh+QMNGmZAuyeQJTH9ErW57OWSvdtuwfBKdS254E=
 "
 mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 nix build .
